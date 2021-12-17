2af5db1e31f7f2289063009dc05434d40ed3bf92
Rauchendraser <andreasr@ppedv.de>
2021-12-17 15:18:09 +0100

    Indizes Fallbeispiele Columnsotre

--- TSQL PowerWoche/TSQL PowerWoche/Z_105_Indizes.sql
+++ TSQL PowerWoche/TSQL PowerWoche/Z_105_Indizes.sql
@@ -15,11 +15,11 @@ gut bei Abfragen mit rel. wenig Ergebniszeilen 1%..nicht fix
 
 ----------------------------------------------
 /*
-! zusammengesetzter IX
-IX mit eingeschl Spalten
-gefilterter IX
+! zusammengesetzter IX (max 16 Spalten, max 900bytes)
+! IX mit eingeschl Spalten (ca 1000 Spalten)
+! gefilterter IX
 ! abdeckender IX -- der ideale IX zur ABfrage kein Lookup kein Scan
-ind Sicht
+ind Sicht --Das Ergebnis der Sicht wird indiziert 
 part IX
 realer hypoth. IX
 ! eindeutiger IX
@@ -62,4 +62,108 @@ select id, freight from ku1 where id < 100 -- Seek
 select id, freight from ku1 where id = 100 --Seek
 
 
+
+
+
+--NCLIX_ID_i_FR
+select id, freight from ku1 where id = 100 --IX 
+
+
+--
+select id, freight, country from ku1 where id < 100  and City = 'london'
+
+
+
+
+
+select productname, sum(unitprice*quantity) as Umsatz from ku1
+where shipcountry = 'Brazil' and freight < 5
+group by productname
+order by Umsatz desc 
+
+
+select productname, sum(unitprice*quantity) as Umsatz
+from ku1
+where shipcountry = 'Brazil' or freight < 5
+group by productname
+order by Umsatz desc 
+
+
+select productname, sum(unitprice*quantity) as Umsatz
+from ku1
+where shipcountry = 'Brazil' and shipcity = 'Rio'
+group by productname
+order by Umsatz desc 
+
+
+set statistics io, time on
+
+select country, count(*) as ANZ from ku1
+group by country
+
+
+create view v1 
+as
+select country, count(*) as ANZ from ku1
+group by country
+
+
+select * from v1
+
+
+select country, count(*) as ANZ from ku1
+group by country
+
+
+alter   view v1 with schemabinding
+as
+select country, count_big(*) as ANZ from dbo.ku1
+group by country
+
+
+2000000 Trd DS Sicht ind
+
+--LAND UMSATZ--> 2 Seiten
+
+
+
+select freight, shipcountry from ku1
+where id<1000 and Shipcountry = 'USA'
+
+
+NCLIX_filterUSA
+
+select freight, shipcountry from ku1
+where id<1000 and Shipcountry = 'Germany'
+
+select freight, shipcountry from ku1
+where id<1000 and Shipcountry = 'USA'
+
+--ist der gefilterte als der ungefilterte
+
+
+select * into ku2 from ku1
+
+select top 3 * from ku2
+
+--Berechnung pro .. where 
+
+--idealer IX
+--Anzahl der Bestellungen pro Land und Kunde , die wo "Outback Lager" bestellt haben
+select shipcountry ,companyname, count(*) as anz 
+from ku1
+where freight < 10 --ProductName = 'Outback Lager'
+group by shipcountry, companyname
+--NCLIX_PName_i_scy_cn
+--, CPU-Zeit = 16 ms, verstrichene Zeit = 14 ms. ...241
+
+
+select shipcountry ,companyname, count(*) as anz 
+from ku2
+where freight <10 --ProductName = 'Outback Lager'
+group by shipcountry ,companyname
+
+
+--a) stimmt  b) stimmt nicht
+--a--> aus 300MB ---> 4 MB---> 3,2MB --> RAM
 
